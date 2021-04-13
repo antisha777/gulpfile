cc25895d722abf8f700756053ca9a7603c5a94bd
antisha777 <61730162+antisha777@users.noreply.github.com>
2021-04-13 10:28:21 +0700

    commit gulp

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var localhost  = 'localhost/doskamel.loc';
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(done) {
+    gulp.src('./style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest("./"))
+        .pipe(browserSync.stream());
+
+
+    done();
+});
+
+gulp.task('serve', function(done) {
+
+    browserSync.init({
+        open: 'external',
+        proxy: localhost,
+        
+    });
+
+    gulp.watch('./*.scss', gulp.series('sass'));
+    gulp.watch("**/*.php").on('change', () => {
+      browserSync.reload();
+      done();
+    });
+  
+
+    done();
+});
+
+gulp.task('default', gulp.series('sass', 'serve'));
